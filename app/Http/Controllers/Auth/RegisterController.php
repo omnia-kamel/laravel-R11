279a019dcf4310ebbca198a4b7cb6ce7ac605398
omnia-kamel <yoyokamel29@gmail.com>
2024-01-10 00:22:23 +0200

    session15

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,6 +70,8 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'mobile' => $data['mobile'],
             'password' => Hash::make($data['password']),
+            'expired' => 0,
+
         ]);
     }
 }
